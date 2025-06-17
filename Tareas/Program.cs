6b2daa31b1edcd2c7eab13830153961a53473504
Nico <nikolasleonard@gmail.com>
2025-06-17 13:10:08 -0300

    terminada peticion a la api y listado de tareas

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+List<Tarea> ListaDeTareas = await GetTareasAsync();
+
+foreach (var tarea in ListaDeTareas)
+{
+    Console.WriteLine($"Tarea: {tarea.id} - Descripcion: {tarea.title}");
+}
+
+static async Task<List<Tarea>> GetTareasAsync()
+{
+    var url = "https://jsonplaceholder.typicode.com/todos/";
+
+    try
+    {
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        List<Tarea> listaTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+        return listaTareas;
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine("Problemas de acceso a la API");
+        Console.WriteLine("Message :{0} ", e.Message);
+        return null;
+    }
+}
